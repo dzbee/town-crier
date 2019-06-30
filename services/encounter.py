6a91d89a06ffc9187cc7a6949c85684b67e7ec23
Dylan Bargteil <dylan.bargteil@gmail.com>
2019-06-30 00:30:30 -0400

    encounter navigation functionality

--- services/encounter.py
+++ services/encounter.py
@@ -1,17 +1,21 @@
-from flask import request, render_template
+from flask import request, render_template, redirect
 from flask_wtf import FlaskForm
-from wtforms import SelectField, validators
+from wtforms import SelectField, SubmitField, validators
+
+from models.encounter import Scenario
 
 class EncounterForm(FlaskForm):
-    action = SelectField(validators=[validators.DataRequired()])
+    action = SelectField(validators=[validators.DataRequired()], coerce=int)
+    submit = SubmitField("Submit")
 
 def register_encounter_endpoints(app):
 
-    @app.route('/encounter')
-    def encounter():
+    @app.route('/encounter/<scenario_id>', methods=["GET", "POST"])
+    def encounter(scenario_id):
+        scenario = Scenario.query.get(scenario_id)
         form = EncounterForm(request.form)
-        form.action.choices = [('hello world', 0)]
+        form.action.choices = [(choice.target_idn, choice.text) for choice in scenario.choices]
         if request.method == 'POST' and form.validate():
-            return 'hello world'
+            return redirect('/encounter/{}'.format(form.action.data))
 
-        return render_template('form.html', form=form)
+        return render_template('encounter.html', scenario_text=scenario.text, form=form)
