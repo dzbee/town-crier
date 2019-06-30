c58cbeb2a2736621aaf1694e6f8be5ee1c6dba9f
Dylan Bargteil <dylan.bargteil@gmail.com>
2019-06-29 20:11:36 -0400

    prevent circular import

--- app.py
+++ app.py
@@ -5,16 +5,16 @@ from flask_bootstrap import Bootstrap
 
 from config import Config
 
-from services.index import register_index_endpoints
-from services.encounter import register_encounter_endpoints
-from services.character import register_character_endpoints
-
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
 Migrate(app, db)
 Bootstrap(app)
+
+from services.index import register_index_endpoints
+from services.encounter import register_encounter_endpoints
+from services.character import register_character_endpoints
 
 register_index_endpoints(app)
 register_encounter_endpoints(app)
